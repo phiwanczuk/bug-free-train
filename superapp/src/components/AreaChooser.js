3fd9223a5fec148b7c4ce38b13aa90725a6a2aa2
Piotrek <phiwanczuk@gmail.com>
2018-02-09 11:36:56 +0100

    random area name chooser added

--- superapp/src/components/AreaChooser.js
+++ superapp/src/components/AreaChooser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {getAreaName} from '../helpers'
 
 
 class AreaChooser extends React.Component{
@@ -8,7 +8,7 @@ class AreaChooser extends React.Component{
             <div>
                 <form className="area-choosers">
                     <h2>Please enter an area name</h2>
-                    <input type="text" required placeholder="Area Name"/>
+                    <input type="text" required placeholder="Area Name" defaultValue={getAreaName()}/>
                     <button type="submit">Choose Area</button>
                 </form>
             </div>
