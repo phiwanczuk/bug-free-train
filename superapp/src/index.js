13e6ee96efb7118ae8da32164320756b6938b83e
Piotrek <phiwanczuk@gmail.com>
2018-02-09 11:22:03 +0100

    miss match components created and applied

--- superapp/src/index.js
+++ superapp/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import App from './components/App'
 import AreaChooser from './components/AreaChooser';
+import NotFound from './components/NotFound'
 
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+            <Match exactly pattern="/" component={AreaChooser}/>
+            <Match pattern="/store/:storeId" component={App}/>
+             <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
